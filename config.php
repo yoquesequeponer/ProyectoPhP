463a96cb6e7500680cc70486dd67291e491a4eb9
Alvaro <daw092021@gmail.com>
2021-03-31 12:43:06 +0200

    codigo base

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+// Define DB Params
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_NAME", "shareboard");
+
+// Define URL
+define("ROOT_PATH", "/2Curso/2º_curso/5Entrega/");
+define("ROOT_URL", "http://localhost:8080/2Curso/2º_curso/5Entrega/");
